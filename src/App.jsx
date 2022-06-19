5bef5456f5c78df5bb5611ac8cb6d91b2ee5667e
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-06-18 22:50:06 -0300

    config: inicialização do projeto com vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        autor="Antonia" 
+        conteudo="conteudo1"
+      />
+    </div>
+  )
+}
+
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props){
+    return (
+        <div>
+            <p>{props.conteudo}</p>
+            <p><strong>{props.autor}</strong></p>
+        </div>
+    )
+}
