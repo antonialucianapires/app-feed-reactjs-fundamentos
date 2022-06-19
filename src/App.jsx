d878ee39cff51c53d3518db86d9785b12cfe88f0
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-06-18 23:09:26 -0300

    feat: definição do estilo global

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header/>
       <Post 
         autor="Antonia" 
         conteudo="conteudo1"
